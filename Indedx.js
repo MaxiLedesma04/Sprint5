66a66986707f871d6202a049d59667a6be3121fa
maxledes <maxinglede@gmail.com>
2023-07-23 22:03:21 -0300

    Segundo commit5

--- Indedx.js
+++ Indedx.js
@@ -1,19 +1,38 @@
-const {creatApp} = Vue
+const {createApp} = Vue
 
 const options ={
     data(){
         return{
+            events: null,
+            categorias:[],
+            valorSearch:"",
+            valorCheked:[],
+            filtrado:[],
 
         }
     },
     created(){
         fetch("https://mindhub-xj03.onrender.com/api/amazing")
         .then(resulte => resulte.json())
-        .then(resulte => {})
+        .then(resulte => {
+            this.events = resulte.events
+            this.filtrado = resulte.events
+            this.categorias = [... new Set(resulte.events.filter(event => event.category).map(event=>event.category))]
+            console.log(this.events)
+        })    
         .catch(error => console.error(error))
+    },
+    methods:{
+        fnFiltrado(){
+            this.filtrado = this.events.filter(event=>{
+                return event.name.toLowerCase().includes(this.valorSearch.toLowerCase()) &&
+                (this.valorCheked.includes(event.category) || this.valorCheked.length == 0)
+            })
+
+        },
     }
 }
 
-const app = created(options)
+const app = createApp(options)
 
 app.mount('#app')
